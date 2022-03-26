d038b1a299490adc29d6bbcbf7dc3a1deab52ab1
fjodor <fjodor.doronin@ya.ru>
2022-03-26 18:09:24 +0500

    запуск бота

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello, yandex")
+import telebot
+from config import apytoken
+
+bot = telebot.TeleBot(apytoken)
+
+
+@bot.message_handler(commands=['start'])
+def start(m):
+    bot.send_message(m.chat.id, "Какой фильм вы бы хотели посмотреть")
+
+
+bot.polling(none_stop=True, interval=0)
