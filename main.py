72077ef758264872ca4ec5fed502dffa412e17e1
Andrei24511 <lp24511@mail.ru>
2022-04-02 13:33:52 +0500

    кнопка со списком актеров под фильмом

--- main.py
+++ main.py
@@ -32,11 +32,12 @@ def get_film(m):
     f_name = ''.join(m.text.split()[1:])
     req = find_film(f_name, m.json['from']['id'])
     poster = open('poster.jpg', 'rb')
-    keyboard1 = types.InlineKeyboardMarkup(row_width=2)
-    button1 = types.InlineKeyboardButton('Оценить фильм', callback_data=f'q1{req[1]}')  # в названии кнопки хранится
+    keyboard1 = types.InlineKeyboardMarkup(row_width=3)
+    button1 = types.InlineKeyboardButton('Оценить', callback_data=f'q1{req[1]}')  # в названии кнопки хранится
     button2 = types.InlineKeyboardButton('Подробнее', callback_data=f'q2{req[1]}')  # id фильма к которому она привязана
-    button3 = types.InlineKeyboardButton('Буду смотреть', callback_data=f'q3{req[1]}')
-    keyboard1.add(button1, button2, button3)
+    button3 = types.InlineKeyboardButton('Актёры', callback_data=f'q3{req[1]}')
+    button4 = types.InlineKeyboardButton('Буду смотреть', callback_data=f'q4{req[1]}')
+    keyboard1.add(button1, button2, button3, button4)
     bot.send_photo(m.chat.id, poster)
     bot.send_message(m.chat.id, req[0].format(m.from_user, bot.get_me()), parse_mode='html', reply_markup=keyboard1)
 
@@ -63,7 +64,7 @@ def callback(call):
                 elif call.data[:2] == 'q2':
                     page = wikipedia.summary(f'ID {call.data[2:]}')  # инфа о фильме с wikipedia
                     bot.send_message(call.message.chat.id, page)
-                elif call.data[:2] == 'q3':
+                elif call.data[:2] == 'q4':
                     db_sess = create_session()
                     curr_id = int(call.data[2:])
                     fm = db_sess.query(Film).filter(Film.film_id == curr_id)[0]
@@ -71,6 +72,14 @@ def callback(call):
                     title = fm.loc_title
                     db_sess.commit()
                     bot.send_message(call.message.chat.id, f'Фильм "{title}" добавлен в лист ожидания')
+                elif call.data[:2] == 'q3':
+                    curr_id = int(call.data[2:])
+                    film = moviesDB.get_movie(curr_id)
+                    text = ''
+                    for i, j in enumerate(film['cast']):
+                        text += f'{i + 1}) <b>{j}</b>\n'
+                    bot.send_message(call.message.chat.id, text.format(call.message.from_user, bot.get_me()),
+                                     parse_mode='html')
 
     except Exception:
         bot.send_message(call.message.chat.id, 'Error callback')
